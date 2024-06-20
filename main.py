6d65a6e9641dc2b8c0d7ffc1f4f056a09ddb2654
amstein4920 <amstein4920@gmail.com>
2024-06-20 16:50:46 +0800

    Testing regex

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
+import re
+
 def main():
     with open("books/frankenstein.txt") as frankensteinText:
         contents = frankensteinText.read()
-    print(len(contents.split()))
-    print(character_count(contents))
+    # print(len(contents.split()))
+    # print(character_count(contents))
+    get_title(contents)
 
 def character_count(string):
     results = {}
@@ -13,4 +16,13 @@ def character_count(string):
         else:
             results[character] = 1
     return results
+
+def get_title(text):
+    searchResult = re.search("^\nTitle((?:\n|.)*?)\n$", text)
+    print(searchResult)
+
+def print_report(text):
+    title = get_title(text)
+    print("Report on " + title)
+
 main()
